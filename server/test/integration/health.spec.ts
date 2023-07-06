66e5a89dfc44dfc0be7762330291c7d13e56f3bc
Tiago Taquelim <tiago.taquelim.ferreira@gmail.com>
2023-07-06 21:08:33 +0100

    feat: web skaffold

---
+++ server/test/integration/health.spec.ts
@@ -0,0 +1,19 @@
+import request from "supertest";
+import HttpStatus from "http-status";
+import { createServer } from "../../src/server";
+
+describe("signup", () => {
+  const server = createServer().listen(80);
+
+  afterAll(async () => {
+    server.close();
+  });
+
+  it("should return 200 if the server is up", async () => {
+    await request(server)
+      .get("/v1/health")
+      .send()
+      .expect("ok")
+      .expect(HttpStatus.OK);
+  });
+});
